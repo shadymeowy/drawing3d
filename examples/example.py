95d84569f418bf655dc02639cf0fb98cd41a64b6
shadymeowy <tolgademirdal@gmail.com>
2023-11-24 01:43:56 +0300

    unnecessary example.py complications

--- examples/example.py
+++ examples/example.py
@@ -1,19 +1,29 @@
 from drawing3d import DrawList, Window, EventList
 import numpy as np
+import time
 
-draw_list = DrawList()
-window = Window(720, 720, b"test")
-camera = window.get_camera()
-camera.set_perspective(45.0, 45.0)
+windows = Window(720, 720, b"test"), Window(720, 720, b"test2")
 
-draw_list.style2(1.0, 1.0, 1.0, 1.0, 1.0)
-draw_list.clear()
+camera1 = windows[0].get_camera()
+camera1.set_perspective(45.0, 45.0)
 
-draw_list.style2(1.0, 0.0, 0.0, 1.0, 4.0)
-draw_list.line(0.0, 0.0, 0.0, 1.0, 1.0, 1.0)
+windows[1].controllable = False
+camera2 = windows[1].get_camera()
+camera2.set_orthographic(0.1, 0.1)
+camera2.position = (0., 0.0, 1.0)
+camera2.rotation = (0.0, -np.pi/2, 0.0)
+
+draw_lists = [DrawList(), DrawList()]
+draw_list_bg, draw_list = draw_lists
+
+draw_list_bg.style2(1.0, 1.0, 1.0, 1.0, 1.0)
+draw_list_bg.clear()
+
+draw_list_bg.style2(1.0, 0.0, 0.0, 1.0, 4.0)
+draw_list_bg.line(0.0, 0.0, 0.0, 1.0, 1.0, 1.0)
 
 # cube
-draw_list.style2(0.0, 1.0, 0.0, 1.0, 4.0)
+draw_list_bg.style2(0.0, 1.0, 0.0, 1.0, 4.0)
 lines = np.array([
     [[0.0, 0.0, 0.0], [0.0, 1.0, 0.0]],
     [[0.0, 0.0, 0.0], [1.0, 0.0, 0.0]],
@@ -28,23 +38,47 @@ lines = np.array([
     [[0.0, 0.0, 1.0], [1.0, 0.0, 1.0]],
     [[0.0, 0.0, 1.0], [0.0, 1.0, 1.0]],
 ])
-draw_list.lines(lines)
+draw_list_bg.lines(lines)
 
 t = np.linspace(0, 2 * np.pi, 100)
 x = np.cos(t)
 y = np.sin(t)
 z = np.zeros_like(t)
 points = np.stack((x, y, z), axis=1)
-draw_list.style2(0.0, 0.0, 1.0, 1.0, 5.0)
-draw_list.polyline(points)
+draw_list_bg.style2(0.0, 0.0, 1.0, 1.0, 5.0)
+draw_list_bg.polyline(points)
 
 event_list = EventList()
+stime = time.time()
+prev_t = 0.0
+
 while not event_list.poll():
-    window.clear()
-    window.render(draw_list)
-    window.handle_events(event_list)
+    t = time.time() - stime
+    dt = t - prev_t
+    prev_t = t
+    print('FPS: {:.2f}'.format(1.0 / dt))
+    camera1.add_rotation(0.0, 0.0, dt / 10)
+
+    draw_list.empty()
+    draw_list.style2(0.0, 0.0, 0.0, 1.0, 5.0)
+    draw_list.line(0.0, 0.0, 0.0, np.cos(t), np.sin(t), 0.0)
+
+    t2 = np.linspace(0, 2 * np.pi, 512)
+    # polar flower
+    x = np.cos(t2 + t) * (2 + np.cos(t2 * 6)) * (1.25 + np.cos(t))
+    y = np.sin(t2 + t) * (2 + np.cos(t2 * 6)) * (1.25 + np.cos(t))
+    z = np.zeros_like(t2)
+    points = np.stack((x, y, z), axis=1)
+    draw_list.style2(1.0, 0.0, 1.0, 1.0, 5.0)
+    draw_list.polyline(points)
+
+    for window in windows:
+        window.renders(draw_lists)
+        window.handle_events(event_list)
     event_list.reset()
 
-window.destroy()
-draw_list.destroy()
-event_list.destroy() 
+
+for window in windows:
+    window.destroy()
+draw_list_bg.destroy()
+event_list.destroy()
