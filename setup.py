6c0e840c9c44a61f9eda3da3878525c0210e7791
shadymeowy <tolgademirdal@gmail.com>
2024-04-08 01:22:57 +0300

    clean builds correctly when building wheels

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 import subprocess
 
+subprocess.run(["make", "clean"])
+subprocess.run(["rm", "drawing3d/_drawing3d.abi3.so"])
 subprocess.run(["make", "all"])
 
 setup(
