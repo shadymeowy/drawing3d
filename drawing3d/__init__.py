5831161732fa54ba27d1f79be3476dfaf1d5da22
shadymeowy <tolgademirdal@gmail.com>
2024-04-08 04:05:17 +0300

    add fallback version information

--- drawing3d/__init__.py
+++ drawing3d/__init__.py
@@ -3,3 +3,9 @@ from .drawlist import DrawList
 from .eventlist import EventList
 from .window import Window
 from .simple3d import Simple3D
+
+try:
+    from .version import version as __version__
+except ImportError:
+    # Fallback version if version.py is not generated
+    __version__ = "0.0.0.dev0"
